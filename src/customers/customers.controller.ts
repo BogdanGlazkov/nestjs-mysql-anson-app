eaf6656c2bc42bc90210e6fe06553d37d233d3f7
BogdanGlazkov <bglazkov@i.ua>
2023-06-16 20:55:27 +0300

    Add GET by id method

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Req,
+  Res,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { CustomersService } from './customers.service';
+import { Request, Response } from 'express';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private customersService: CustomersService) {}
 
-  @Get()
-  getCustomer() {
-    return this.customersService.findCustomer();
+  @Get(':id')
+  getCustomer(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const customer = this.customersService.findCustomerById(id);
+    if (customer) {
+      res.send(customer);
+    } else {
+      res.status(400).send({ message: 'Customer not found' });
+    }
+  }
+
+  @Get('search/:id')
+  searchCustomerById(@Param('id', ParseIntPipe) id: number) {
+    const customer = this.customersService.findCustomerById(id);
+    if (customer) return customer;
+    else throw new HttpException('Customer not found', HttpStatus.BAD_REQUEST);
   }
 }
--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-  findCustomer() {
-    return {
-      id: 1,
-      email: 'daniel@test.com',
-      createdAt: new Date(),
-    };
+  users = [
+    { id: 1, email: 'daniel@test.com', createdAt: new Date() },
+    { id: 2, email: 'adam@test.com', createdAt: new Date() },
+    { id: 3, email: 'spencer@test.com', createdAt: new Date() },
+  ];
+
+  findCustomerById(id: number) {
+    return this.users.find((user) => user.id === id);
   }
 }

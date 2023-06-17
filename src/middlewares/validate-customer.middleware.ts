0f82aee0f246d78d12a83977bcb2da195ddbdba4
BogdanGlazkov <bglazkov@i.ua>
2023-06-17 17:03:25 +0300

    Add validation middleware

---
+++ src/middlewares/validate-customer.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class ValidateCustomerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const { authorization } = req.headers;
+    if (!authorization)
+      res.status(403).send({ error: 'No authentication token provided' });
+
+    if (authorization === '123') next();
+    else res.status(403).send({ error: 'Invalid authentication token' });
+  }
+}

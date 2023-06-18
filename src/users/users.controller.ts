225c55f4443de45a492d1ef077a1a15cb5f5f56e
BogdanGlazkov <bglazkov@i.ua>
2023-06-18 17:13:03 +0300

    Add HttpException filter

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,10 +8,12 @@ import {
   UseInterceptors,
   ClassSerializerInterceptor,
   ParseIntPipe,
+  UseFilters,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { SerializedUser } from 'src/types/User';
 import { UserNotFoundExcepton } from 'src/exceptions/UserNotFound.exception';
+import { HttpExceptionFilter } from 'src/filters/HttpException.filter';
 
 @Controller('users')
 export class UsersController {
@@ -34,6 +36,7 @@ export class UsersController {
   }
 
   @UseInterceptors(ClassSerializerInterceptor)
+  @UseFilters(HttpExceptionFilter)
   @Get('/id/:id')
   getById(@Param('id', ParseIntPipe) id: number) {
     const user = this.usersService.getUserById(id);

28f2595edb547be171e1e9696be5bd5fadb79c24
BogdanGlazkov <bglazkov@i.ua>
2023-06-18 23:00:41 +0300

    Add auth POST method

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/typeorm';
+import { LocalStrategy } from 'src/utils/LocalStrategy';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User]), PassportModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    {
+      provide: 'AUTH_SERVICE',
+      useClass: AuthService,
+    },
+    {
+      provide: 'USER_SERVICE',
+      useClass: UsersService,
+    },
+    LocalStrategy,
+  ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject('USER_SERVICE') private readonly usersService: UsersService,
+  ) {}
+
+  async validateUser(username: string, password: string) {
+    const userDB = await this.usersService.findUserByUsername(username);
+    if (userDB && userDB.password === password) return userDB;
+    else return null;
+  }
+}

e03a20364871a58b81b9d6126cc8076098fc6af7
BogdanGlazkov <bglazkov@i.ua>
2023-06-19 17:40:32 +0300

    Add session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(
+    session({
+      name: 'NESTJS_SESSION_ID',
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
   await app.listen(5000);
 }
 bootstrap();

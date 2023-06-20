b1598390d4c4129f197b9dbc4b79ea7d48b62f67
BogdanGlazkov <bglazkov@i.ua>
2023-06-20 23:46:36 +0300

    Add createUser E2E test part 2

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -15,14 +15,55 @@ describe('UsersController E2E Test', () => {
     await app.init();
   });
 
-  it('should create a new user', () => {
-    return request(app.getHttpServer())
-      .post('/users/create')
-      .send({
-        username: 'Testuser',
-        email: 'testuser@test.com',
-        password: '12345678',
-      })
-      .expect(201);
+  describe('Creating New Users POST /api/users/create', () => {
+    const CREATE_USER_URL = '/users/create';
+
+    it('should create a new user', () => {
+      return request(app.getHttpServer())
+        .post(CREATE_USER_URL)
+        .send({
+          username: 'Testuser',
+          email: 'testuser@test.com',
+          password: '12345678',
+        })
+        .expect(201);
+    });
+
+    it('should return 400 when invalid username is provided', () => {
+      return request(app.getHttpServer())
+        .post(CREATE_USER_URL)
+        .send({
+          username: 'Te',
+          email: 'testuser@test.com',
+          password: '12345678',
+        })
+        .expect(400);
+    });
+
+    it('should return 400 when invalid password is provided', () => {
+      return request(app.getHttpServer())
+        .post(CREATE_USER_URL)
+        .send({
+          username: 'Testuser',
+          email: 'testuser@test.com',
+          password: '12',
+        })
+        .expect(400);
+    });
+
+    it('should return 400 when invalid email is provided', () => {
+      return request(app.getHttpServer())
+        .post(CREATE_USER_URL)
+        .send({
+          username: 'Testuser',
+          email: 'testuser',
+          password: '12345678',
+        })
+        .expect(400);
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
